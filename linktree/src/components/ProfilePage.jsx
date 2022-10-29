2829fe6df5e414337e026e005ab5c4220ef7b07a
Nkwor-Jane <nkwor.jane.chinelo@gmail.com>
2022-10-29 21:59:09 +0100

    Completed most of the CSS for the page-- Some minor details left

--- linktree/src/components/ProfilePage.jsx
+++ linktree/src/components/ProfilePage.jsx
@@ -1,28 +1,40 @@
 import React from "react";
-import "../styles/ProfilePage.css"
+import "../styles/ProfilePage.css";
+import ProfileImage from "../styles/profile__img.png";
 
 export default function ProfilePage() {
     return (
-        <div>
-            <div>
-                an icon
+        <div className="profile">
+            <div className="icon">
+
             </div>
             <div className="image">
-                <img src="" alt="Face" id="profile_img" />
+                <img src={ProfileImage} alt="Face" id="profile_img" />
             </div>
-            <h3>Nkwor Jane</h3>
+            <h3>Annette Black</h3>
 
             <div className="btns">
-                <div>
-                
+                <div className="twitter-btn">
+                    <button className="twitter" id="twitter">Twitter Link</button>
                 </div>
-                <button className="twitter" id="twitter">Twitter Link</button>
-                <button className="slack" id="slack">Slack Link</button>
-                <a href="https://training.zuri.team/" className="team" id="btn__zuri">Zuri Team</a>
-                <a href="http://books.zuri.team/" className="books" id="books">Zuri Books</a>
-                <a href="https://books.zuri.team/" className="python" id="book_python">Python Books</a>
-                <a href="https://background.zuri.team/" className="check" id="pitch">Background Check for coders</a>
-                <a href="https://books.zuri.team/design-rules" className="design" id="book_design">Design Books</a>
+                <div className="slack">
+                    <button id="slack">Slack Link</button>
+                </div>
+                <div className="links">
+                    <a href="https://training.zuri.team/" className="team" id="btn__zuri" target="_blank" rel="noreferrer noopener">Zuri Team</a>
+                </div>
+                <div className="links">
+                    <a href="http://books.zuri.team/" className="books" id="books" target="_blank" rel="noreferrer noopener">Zuri Books</a> 
+                </div>
+                <div className="links">
+                    <a href="https://books.zuri.team/" className="python" id="book_python" target="_blank" rel="noreferrer noopener">Python Books</a>
+                </div>
+                <div className="links">
+                    <a href="https://background.zuri.team/" className="check" id="pitch" target="_blank" rel="noreferrer noopener">Background Check for coders</a>
+                </div>
+                <div className="links">
+                    <a href="https://books.zuri.team/design-rules" className="design" id="book_design" target="_blank" rel="noreferrer noopener">Design Books</a>
+                </div>
             </div>
         </div>
     )
